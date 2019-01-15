8466550122396b7960f49813bb0c579a1342055c
Tomas Tombakas <tombakas@gmail.com>
2019-01-16 00:51:01 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+from flask import Flask
+from flask import g
+
+DATABASE = './ga2-movies.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
