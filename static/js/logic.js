88016990b9c5ae85ab6972b571b8e209dcc68770
Tomas Tombakas <tombakas@gmail.com>
2019-01-17 13:57:25 +0100

    Improve bar style

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,3 @@
-let d;
-
 // Gross Per Genre
 $("#grossPerGenre").on("click", function() {
   $.getJSON("/grossPerGenre", function( data ) {
@@ -16,12 +14,21 @@ $("#grossPerGenre").on("click", function() {
 
     d3.select(".visualization").selectAll("p").remove();
 
-    d3.select(".visualization")
+    let bars = d3.select(".visualization")
       .selectAll("div")
       .data(data)
       .enter()
       .append("div")
+      .classed("bar-container", true);
+
+    bars.data(data).append("span").text(d => d[0]);
+    bars.data(data).append("div")
       .classed("bar-style", true)
-      .style("width", d => (d[1] / maxWorldwide * 1000 + "px")).text(d => d[0]);
+      .style("width", d => (d[1] / maxWorldwide * 1000 + "px"));
+
+    bars.data(data).append("span")
+      .classed("bar-value", true)
+      .text(d => "$ " + d[1]);
+      
   })
 });
