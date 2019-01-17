a2c8da0739195c00e78740c14305f8f41fb1b65a
Tomas Tombakas <tombakas@gmail.com>
2019-01-17 14:29:14 +0100

    Further improve bar graph, numeral format

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,34 +1,29 @@
 // Gross Per Genre
 $("#grossPerGenre").on("click", function() {
   $.getJSON("/grossPerGenre", function( data ) {
-    d = data;
+    let sortedData = data.sort((a, b) => a[1] < b[1]);
 
     let maxDomestic = Math.max(...(data.map(row => row[1])));
     let maxWorldwide = Math.max(...(data.map(row => row[2])));
     let max = Math.max(maxDomestic, maxWorldwide);
-    console.log(data.map(row => row[1]));
-    console.log(maxDomestic);
-    console.log(data.map(row => row[2]));
-    console.log(maxWorldwide);
-    console.log(max);
 
     d3.select(".visualization").selectAll("p").remove();
 
     let bars = d3.select(".visualization")
       .selectAll("div")
-      .data(data)
+      .data(sortedData)
       .enter()
       .append("div")
       .classed("bar-container", true);
 
-    bars.data(data).append("span").text(d => d[0]);
-    bars.data(data).append("div")
+    bars.data(sortedData).append("span").text(d => d[0]);
+    bars.data(sortedData).append("div")
       .classed("bar-style", true)
       .style("width", d => (d[1] / maxWorldwide * 1000 + "px"));
 
-    bars.data(data).append("span")
+    bars.data(sortedData).append("span")
       .classed("bar-value", true)
-      .text(d => "$ " + d[1]);
-      
+      .text(d => "$ " + numeral(d[1]).format("0,0"));
+
   })
 });
